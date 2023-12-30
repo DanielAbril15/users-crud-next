8c749ef66b07eb6d860a70d4fe607a378a488a80
Daniel Abril <danbril1015@gmail.com>
2023-12-30 17:51:06 -0500

    disclaimer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,25 @@ export default function Home() {
   return (
     <main className="content">
       <CreateForm />
+
+      <h4 className="text-center disclaimer">
+        <strong>Disclaimer:</strong>
+        <p>
+          La base de datos puede demorar en responder, te recomiendo visitar la
+          ruta <br />
+          <u>
+            <a
+              target="_blank"
+              href="https://users-crud-k0xm.onrender.com/api/users/"
+            >
+              https://users-crud-k0xm.onrender.com/api/users/
+            </a>{" "}
+            <br />
+          </u>
+          de la db. Luego de realizarce la primera peticion la app funcionara
+          con normalidad.
+        </p>
+      </h4>
     </main>
   );
 }
